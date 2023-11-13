d86bd3c106a9f8fc15b8595f533d2fedd721413c
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-13 15:15:59 +0300

    :sparkles: Add datastore extension utils

--- app/src/main/java/com/example/foodie/utils/Extensions.kt
+++ app/src/main/java/com/example/foodie/utils/Extensions.kt
@@ -1,8 +1,14 @@
 package com.example.foodie.utils
 
+import android.content.Context
 import android.view.View
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.navigation.Navigation
 
 fun Navigation.changePage(view: View, id: Int) {
     findNavController(view).navigate(id)
 }
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore("account")
