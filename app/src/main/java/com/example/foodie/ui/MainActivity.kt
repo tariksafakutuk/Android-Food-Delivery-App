0bc914998a6e767f6306b989a7cd764102165d76
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-13 19:16:44 +0300

    :sparkles: Add fragment navigation to bottom nav items

--- app/src/main/java/com/example/foodie/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodie/ui/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.foodie.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.foodie.R
+import com.example.foodie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navHostFragment.navController)
+
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            when(destination.id) {
+                R.id.loginFragment, R.id.signUpFragment -> binding.bottomNavigationView.visibility = View.GONE
+                else -> binding.bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
 }
