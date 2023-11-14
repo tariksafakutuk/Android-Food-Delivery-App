1ee0b6b70a9c062ce2883a7e7b10a19341ee30e0
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-14 15:51:45 +0300

    :lipstick: Add argument to navigation graph file

--- app/src/main/java/com/example/foodie/data/entity/Food.kt
+++ app/src/main/java/com/example/foodie/data/entity/Food.kt
@@ -1,7 +1,9 @@
 package com.example.foodie.data.entity
 
+import java.io.Serializable
+
 data class Food(var foodId: Int,
                 var foodName: String,
                 var foodImageName: String,
-                var foodPrice: Int) {
+                var foodPrice: Int): Serializable {
 }
