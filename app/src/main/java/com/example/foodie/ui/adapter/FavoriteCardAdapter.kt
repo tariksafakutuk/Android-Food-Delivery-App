b9da6def50dfb8993eb90688ae44f393b479edd6
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-14 20:34:42 +0300

    :sparkles: Add page transition feature to favorite card

--- app/src/main/java/com/example/foodie/ui/adapter/FavoriteCardAdapter.kt
+++ app/src/main/java/com/example/foodie/ui/adapter/FavoriteCardAdapter.kt
@@ -4,9 +4,13 @@ import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.example.foodie.R
 import com.example.foodie.data.entity.FavoriteFood
 import com.example.foodie.databinding.FavoriteCardBinding
+import com.example.foodie.ui.fragment.FavoritesFragmentDirections
+import com.example.foodie.utils.changePage
 
 class FavoriteCardAdapter(
     private var mContext: Context,
@@ -34,6 +38,10 @@ class FavoriteCardAdapter(
 
         binding.ivFavoriteButton.setOnClickListener {
             Log.e("Message", "Delete favorite food")
+        }
+
+        binding.buttonFavoritesCart.setOnClickListener {
+            Navigation.changePage(it, R.id.action_favoritesFragment_to_cartFragment)
         }
     }
 
