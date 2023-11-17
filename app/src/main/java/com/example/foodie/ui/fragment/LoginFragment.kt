e2ccf3df19bdeaa9f8837a690a7c2fa66075e65f
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-17 19:28:47 +0300

    :sparkles: Add data binding feature to login fragment

--- app/src/main/java/com/example/foodie/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/foodie/ui/fragment/LoginFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.foodie.R
@@ -18,14 +20,19 @@ import kotlinx.coroutines.launch
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var loginPref: LoginPref
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.bg_page)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
 
-        val loginPref = LoginPref(requireContext())
+        binding.loginFragment = this
+        binding.loginStatus = "loading"
+
+        loginPref = LoginPref(requireContext())
 
         CoroutineScope(Dispatchers.Main).launch {
             val email = loginPref.readEmail()
@@ -33,65 +40,39 @@ class LoginFragment : Fragment() {
             val password = loginPref.readPassword()
 
             if ((email == "test@gmail.com" || username == "test") && password == "1234") {
-                binding.ivLogin.visibility = View.GONE
-                binding.tvLogin.visibility = View.GONE
-                binding.tvAccountLogin.visibility = View.GONE
-                binding.accountLoginLayout.visibility = View.GONE
-                binding.tvPasswordLogin.visibility = View.GONE
-                binding.passwordLoginLayout.visibility = View.GONE
-                binding.buttonLogin.visibility = View.GONE
-                binding.tvSignUpClick.visibility = View.GONE
-                binding.tvProgressLogin.visibility = View.VISIBLE
-                binding.progressBarLogin.visibility = View.VISIBLE
-
+                binding.loginStatus = "login"
                 delay(2000)
-
-                clickView(binding.tvLogin, R.id.action_loginFragment_to_homePageFragment)
-            }
-        }
-
-        binding.buttonLogin.setOnClickListener {
-            val account = binding.editTextAccountLogin.text.toString()
-            val password = binding.editTextPasswordLogin.text.toString()
-
-            if (account == "test" || account == "test@gmail.com") {
-                if (password == "1234") {
-                    CoroutineScope(Dispatchers.Main).launch {
-                        loginPref.createEmail("test@gmail.com")
-                        loginPref.createUsername("test")
-                        loginPref.createPassword("1234")
-
-                        binding.ivLogin.visibility = View.GONE
-                        binding.tvLogin.visibility = View.GONE
-                        binding.tvAccountLogin.visibility = View.GONE
-                        binding.accountLoginLayout.visibility = View.GONE
-                        binding.tvPasswordLogin.visibility = View.GONE
-                        binding.passwordLoginLayout.visibility = View.GONE
-                        binding.buttonLogin.visibility = View.GONE
-                        binding.tvSignUpClick.visibility = View.GONE
-                        binding.tvProgressLogin.visibility = View.VISIBLE
-                        binding.progressBarLogin.visibility = View.VISIBLE
-
-                        delay(2000)
-
-                        clickView(it, R.id.action_loginFragment_to_homePageFragment)
-                    }
-                } else {
-                    Snackbar.make(it, "Şifre Hatalı", Snackbar.LENGTH_SHORT).show()
-                }
+                Navigation.changePage(binding.tvLogin, R.id.action_loginFragment_to_homePageFragment)
             } else {
-                Snackbar.make(it, "Kullanıcı Adı veya Email Hatalı", Snackbar.LENGTH_SHORT).show()
+                binding.loginStatus = "loginFailed"
             }
-        }
-
-        binding.tvSignUpClick.setOnClickListener {
-            clickView(it, R.id.action_loginFragment_to_signUpFragment)
         }
 
         return binding.root
     }
 
-    fun clickView(view: View, id: Int) {
-        Navigation.changePage(view, id)
+    fun clickLoginButton(view: View, account: String, password: String) {
+        if (account == "test" || account == "test@gmail.com") {
+            if (password == "1234") {
+                CoroutineScope(Dispatchers.Main).launch {
+                    loginPref.createEmail("test@gmail.com")
+                    loginPref.createUsername("test")
+                    loginPref.createPassword("1234")
+
+                    binding.loginStatus = "login"
+                    delay(2000)
+                    Navigation.changePage(view, R.id.action_loginFragment_to_homePageFragment)
+
+                }
+            } else {
+                Snackbar.make(view, "Şifre Hatalı", Snackbar.LENGTH_SHORT).show()
+            }
+        } else {
+            Snackbar.make(view, "Kullanıcı Adı veya Email Hatalı", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    fun clickTextView(view: View) {
+        Navigation.changePage(view, R.id.action_loginFragment_to_signUpFragment)
     }
 }
