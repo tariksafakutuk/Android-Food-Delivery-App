6a4de32cb7c672cdae31c795a997c0f01ee58eb6
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-17 12:09:53 +0300

    :sparkles: Add design links to account detail fragment

--- app/src/main/java/com/example/foodie/ui/fragment/AccountDetailFragment.kt
+++ app/src/main/java/com/example/foodie/ui/fragment/AccountDetailFragment.kt
@@ -1,60 +1,143 @@
 package com.example.foodie.ui.fragment
 
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.navArgs
 import com.example.foodie.R
+import com.example.foodie.databinding.FragmentAccountDetailBinding
+import com.example.foodie.datastore.LoginPref
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AccountDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentAccountDetailBinding
+    private lateinit var loginPref: LoginPref
+    private val bundle: AccountDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_detail, container, false)
-    }
+    ): View {
+        binding = FragmentAccountDetailBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AccountDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        binding.ivAccountBack.setOnClickListener {
+            goToPreviousPage()
+        }
+
+        loginPref = LoginPref(requireContext())
+
+        binding.editTextNew.addTextChangedListener {
+            binding.layoutNew.error = null
+        }
+
+        binding.editTextAgainNew.addTextChangedListener {
+            binding.layoutAgainNew.error = null
+        }
+
+        when (bundle.action) {
+            "email" -> {
+                CoroutineScope(Dispatchers.Main).launch {
+                    binding.tvAcoountDetailTitle.text = "Email Değişikliği"
+                    binding.layoutCurrent.isEnabled = false
+
+                    binding.editTextCurrent.setText(loginPref.readEmail())
+                    binding.layoutCurrent.hint = "Mevcut Email"
+                    binding.layoutNew.hint = "Yeni Email"
+                    binding.layoutAgainNew.hint = "Tekrar Yeni Email"
                 }
             }
+
+            "password" -> {
+                binding.tvAcoountDetailTitle.text = "Şifre Değişikliği"
+
+                binding.editTextCurrent.addTextChangedListener {
+                    binding.layoutCurrent.error = null
+                }
+
+                binding.layoutCurrent.hint = "Mevcut Şifre"
+                binding.editTextCurrent.transformationMethod = PasswordTransformationMethod.getInstance()
+
+                binding.layoutNew.hint = "Yeni Şifre"
+                binding.editTextNew.transformationMethod = PasswordTransformationMethod.getInstance()
+
+                binding.layoutAgainNew.hint = "Tekrar Yeni Şifre"
+                binding.editTextAgainNew.transformationMethod = PasswordTransformationMethod.getInstance()
+
+                binding.layoutCurrent.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+                binding.layoutCurrent.setEndIconDrawable(R.drawable.custom_password_eye)
+
+                binding.layoutNew.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+                binding.layoutNew.setEndIconDrawable(R.drawable.custom_password_eye)
+
+                binding.layoutAgainNew.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+                binding.layoutAgainNew.setEndIconDrawable(R.drawable.custom_password_eye)
+            }
+        }
+
+        binding.buttonUpdateAccount.setOnClickListener {
+            updateAccount(it)
+        }
+
+        return binding.root
+    }
+
+    fun updateAccount(view: View) {
+        when (bundle.action) {
+            "email" -> {
+                if (binding.editTextNew.text.toString() == "") {
+                    binding.layoutNew.error = "Yeni email adresinizi girmelisiniz"
+                } else if (binding.editTextNew.text.toString() == binding.editTextAgainNew.text.toString()) {
+                    Snackbar.make(view, "Email adresinizi değiştirmek istediğinize emin misiniz?", Snackbar.LENGTH_SHORT)
+                        .setAction("Evet") {
+                            CoroutineScope(Dispatchers.Main).launch {
+                                loginPref.createEmail(binding.editTextNew.text.toString())
+                                Snackbar.make(view, "Email adresiniz başarılı bir şekilde değiştirildi", Snackbar.LENGTH_SHORT).show()
+                                goToPreviousPage()
+                            }
+                        }
+                        .show()
+                } else {
+                    binding.layoutAgainNew.error = "Girilen email bilgisi Yeni Email alanındakiyle aynı olmalıdır"
+                }
+            }
+
+            "password" -> {
+                CoroutineScope(Dispatchers.Main).launch {
+                    val password = loginPref.readPassword()
+
+                    if (binding.editTextCurrent.text.toString() == "") {
+                        binding.layoutCurrent.error = "Mevcut şifrenizi girmelisiniz"
+                    } else if (binding.editTextCurrent.text.toString() != password) {
+                        binding.layoutCurrent.error = "Girilen şifre bilgisi yanlış"
+                    } else if (binding.editTextNew.text.toString() == "") {
+                        binding.layoutNew.error = "Yeni şifrenizi girmelisiniz"
+                    } else if (binding.editTextNew.text.toString() == binding.editTextAgainNew.text.toString()) {
+                        Snackbar.make(view, "Şifrenizi değiştirmek istediğinize emin misiniz?", Snackbar.LENGTH_SHORT)
+                            .setAction("Evet") {
+                                CoroutineScope(Dispatchers.Main).launch {
+                                    loginPref.createPassword(binding.editTextNew.text.toString())
+                                    Snackbar.make(view, "Şifreniz başarılı bir şekilde değiştirildi", Snackbar.LENGTH_SHORT).show()
+                                    goToPreviousPage()
+                                }
+                            }
+                            .show()
+                    } else {
+                        binding.layoutAgainNew.error = "Girilen şifreniz Yeni Şifre alanındakiyle aynı olmalıdır"
+                    }
+                }
+            }
+        }
+    }
+
+    fun goToPreviousPage() {
+        requireActivity().onBackPressedDispatcher.onBackPressed()
     }
 }
