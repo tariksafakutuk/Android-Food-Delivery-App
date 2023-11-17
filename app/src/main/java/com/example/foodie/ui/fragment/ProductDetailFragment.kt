ddcc35bf0263d89171b71bf202c9768e2f9c9b8d
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-17 16:12:32 +0300

    :sparkles: Add design links to product detail fragment

--- app/src/main/java/com/example/foodie/ui/fragment/ProductDetailFragment.kt
+++ app/src/main/java/com/example/foodie/ui/fragment/ProductDetailFragment.kt
@@ -5,56 +5,62 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.navArgs
 import com.example.foodie.R
+import com.example.foodie.databinding.FragmentProductDetailBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProductDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProductDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentProductDetailBinding
+    private val bundle: ProductDetailFragmentArgs by navArgs()
+    private var isFavorite = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_detail, container, false)
-    }
+    ): View {
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
+        binding = FragmentProductDetailBinding.inflate(inflater, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProductDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProductDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        isFavorite = bundle.isFavorite
+
+        if (isFavorite) {
+            binding.ivDetailFavoriteButton.setColorFilter(ContextCompat.getColor(requireContext(), R.color.product_detail_item))
+        }
+
+        binding.ivDetailFavoriteButton.setOnClickListener {
+            if (isFavorite) {
+                binding.ivDetailFavoriteButton.setColorFilter(ContextCompat.getColor(requireContext(), R.color.product_detail_favorite))
+            } else {
+                binding.ivDetailFavoriteButton.setColorFilter(ContextCompat.getColor(requireContext(), R.color.product_detail_item))
             }
+
+            isFavorite = !isFavorite
+        }
+
+        binding.ivDetailFood.setImageResource(
+            resources.getIdentifier(bundle.food.foodImageName, "drawable", requireContext().packageName)
+        )
+
+        binding.tvDetailFoodName.text = bundle.food.foodName
+        binding.tvDetailPrice.text = "${bundle.food.foodPrice} TL"
+        binding.tvDetailQuantity.text = "1"
+
+        binding.ivDetailDecrease.setOnClickListener {
+            var lastQuantity = binding.tvDetailQuantity.text.toString().toInt()
+            if (lastQuantity != 1) {
+                lastQuantity -= 1
+                binding.tvDetailQuantity.text = lastQuantity.toString()
+                binding.tvDetailPrice.text = "${lastQuantity * bundle.food.foodPrice} TL"
+            }
+        }
+
+        binding.ivDetailIncrease.setOnClickListener {
+            val lastQuantity = binding.tvDetailQuantity.text.toString().toInt() + 1
+            binding.tvDetailQuantity.text = (lastQuantity).toString()
+            binding.tvDetailPrice.text = "${lastQuantity * bundle.food.foodPrice} TL"
+        }
+
+        return binding.root
     }
 }
