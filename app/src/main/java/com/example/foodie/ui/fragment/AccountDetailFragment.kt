f243d74f7ee2db1e67ad6bc2e91a2cc64d79598a
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-18 12:56:09 +0300

    :sparkles: Add data binding feature to account detail fragment

--- app/src/main/java/com/example/foodie/ui/fragment/AccountDetailFragment.kt
+++ app/src/main/java/com/example/foodie/ui/fragment/AccountDetailFragment.kt
@@ -1,18 +1,16 @@
 package com.example.foodie.ui.fragment
 
 import android.os.Bundle
-import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.navArgs
 import com.example.foodie.R
 import com.example.foodie.databinding.FragmentAccountDetailBinding
 import com.example.foodie.datastore.LoginPref
 import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -26,64 +24,27 @@ class AccountDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentAccountDetailBinding.inflate(inflater, container, false)
-
-        binding.ivAccountBack.setOnClickListener {
-            goToPreviousPage()
-        }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_account_detail, container, false)
+        binding.accountDetailFragment = this
 
         loginPref = LoginPref(requireContext())
-
-        binding.editTextNew.addTextChangedListener {
-            binding.layoutNew.error = null
-        }
-
-        binding.editTextAgainNew.addTextChangedListener {
-            binding.layoutAgainNew.error = null
-        }
 
         when (bundle.action) {
             "email" -> {
                 CoroutineScope(Dispatchers.Main).launch {
-                    binding.tvAcoountDetailTitle.text = "Email Değişikliği"
-                    binding.layoutCurrent.isEnabled = false
-
-                    binding.editTextCurrent.setText(loginPref.readEmail())
-                    binding.layoutCurrent.hint = "Mevcut Email"
-                    binding.layoutNew.hint = "Yeni Email"
-                    binding.layoutAgainNew.hint = "Tekrar Yeni Email"
+                    binding.accountDetailTitle = "Email Değişikliği"
+                    binding.isPassword = false
+                    binding.currentText = loginPref.readEmail()
+                    binding.hintText = "Email"
                 }
             }
 
             "password" -> {
-                binding.tvAcoountDetailTitle.text = "Şifre Değişikliği"
-
-                binding.editTextCurrent.addTextChangedListener {
-                    binding.layoutCurrent.error = null
-                }
-
-                binding.layoutCurrent.hint = "Mevcut Şifre"
-                binding.editTextCurrent.transformationMethod = PasswordTransformationMethod.getInstance()
-
-                binding.layoutNew.hint = "Yeni Şifre"
-                binding.editTextNew.transformationMethod = PasswordTransformationMethod.getInstance()
-
-                binding.layoutAgainNew.hint = "Tekrar Yeni Şifre"
-                binding.editTextAgainNew.transformationMethod = PasswordTransformationMethod.getInstance()
-
-                binding.layoutCurrent.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
-                binding.layoutCurrent.setEndIconDrawable(R.drawable.custom_password_eye)
-
-                binding.layoutNew.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
-                binding.layoutNew.setEndIconDrawable(R.drawable.custom_password_eye)
-
-                binding.layoutAgainNew.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
-                binding.layoutAgainNew.setEndIconDrawable(R.drawable.custom_password_eye)
+                binding.accountDetailTitle = "Şifre Değişikliği"
+                binding.isPassword = true
+                binding.hintText = "Şifre"
+                binding.passwordEyeDrawable = R.drawable.custom_password_eye
             }
-        }
-
-        binding.buttonUpdateAccount.setOnClickListener {
-            updateAccount(it)
         }
 
         return binding.root
@@ -139,5 +100,9 @@ class AccountDetailFragment : Fragment() {
 
     fun goToPreviousPage() {
         requireActivity().onBackPressedDispatcher.onBackPressed()
+    }
+
+    fun errorDisable() {
+        binding.isErrorEnabled = false
     }
 }
