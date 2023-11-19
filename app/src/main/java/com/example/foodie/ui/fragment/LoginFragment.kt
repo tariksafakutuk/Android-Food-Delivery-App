f528ff2cefc8f7b422e08df32a46ea975cbc4db2
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-19 14:03:39 +0300

    :building_construction: Bind ViewModel to login fragment

--- app/src/main/java/com/example/foodie/ui/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/foodie/ui/fragment/LoginFragment.kt
@@ -7,10 +7,12 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import com.example.foodie.R
 import com.example.foodie.databinding.FragmentLoginBinding
 import com.example.foodie.datastore.LoginPref
+import com.example.foodie.ui.viewmodel.LoginViewModel
 import com.example.foodie.utils.changePage
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.CoroutineScope
@@ -21,6 +23,7 @@ import kotlinx.coroutines.launch
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
     private lateinit var loginPref: LoginPref
+    private lateinit var viewModel: LoginViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +31,7 @@ class LoginFragment : Fragment() {
     ): View {
         requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.bg_page)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
-
         binding.loginFragment = this
-        binding.loginStatus = "loading"
 
         loginPref = LoginPref(requireContext())
 
@@ -39,37 +40,51 @@ class LoginFragment : Fragment() {
             val username = loginPref.readUsername()
             val password = loginPref.readPassword()
 
-            if ((email == "test@gmail.com" || username == "test") && password == "1234") {
-                binding.loginStatus = "login"
-                delay(2000)
-                Navigation.changePage(binding.tvLogin, R.id.action_loginFragment_to_homePageFragment)
-            } else {
-                binding.loginStatus = "loginFailed"
+            viewModel.isLoginCheck(email, username, password)
+        }
+
+        viewModel.loginStatus.observe(viewLifecycleOwner) {
+            CoroutineScope(Dispatchers.Main).launch {
+                binding.loginStatus = it
+
+                if (it == "login") {
+                    delay(2000)
+                    Navigation.changePage(binding.tvLogin, R.id.action_loginFragment_to_homePageFragment)
+                }
+            }
+        }
+
+        viewModel.userData.observe(viewLifecycleOwner) {
+            when (it[0]) {
+                "passwordFailed" -> {
+                    Snackbar.make(binding.tvLogin, "Şifre Hatalı", Snackbar.LENGTH_SHORT).show()
+                }
+
+                "accountFailed" -> {
+                    Snackbar.make(binding.tvLogin, "Kullanıcı Adı veya Email Hatalı", Snackbar.LENGTH_SHORT).show()
+                }
+
+                else -> {
+                    CoroutineScope(Dispatchers.Main).launch {
+                        loginPref.createEmail(it[0])
+                        loginPref.createUsername(it[1])
+                        loginPref.createPassword(it[2])
+                    }
+                }
             }
         }
 
         return binding.root
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tempViewModel: LoginViewModel by viewModels()
+        viewModel = tempViewModel
+    }
+
     fun clickLoginButton(view: View, account: String, password: String) {
-        if (account == "test" || account == "test@gmail.com") {
-            if (password == "1234") {
-                CoroutineScope(Dispatchers.Main).launch {
-                    loginPref.createEmail("test@gmail.com")
-                    loginPref.createUsername("test")
-                    loginPref.createPassword("1234")
-
-                    binding.loginStatus = "login"
-                    delay(2000)
-                    Navigation.changePage(view, R.id.action_loginFragment_to_homePageFragment)
-
-                }
-            } else {
-                Snackbar.make(view, "Şifre Hatalı", Snackbar.LENGTH_SHORT).show()
-            }
-        } else {
-            Snackbar.make(view, "Kullanıcı Adı veya Email Hatalı", Snackbar.LENGTH_SHORT).show()
-        }
+        viewModel.login(account, password)
     }
 
     fun clickTextView(view: View) {
--- app/src/main/java/com/example/foodie/ui/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/foodie/ui/viewmodel/LoginViewModel.kt
@@ -1,6 +1,34 @@
 package com.example.foodie.ui.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class LoginViewModel: ViewModel() {
+class LoginViewModel(): ViewModel() {
+    val loginStatus = MutableLiveData<String>()
+    val userData = MutableLiveData<List<String>>()
+
+    init {
+        loginStatus.value = "loading"
+    }
+
+    fun isLoginCheck(email: String, username: String, password: String) {
+        if ((email == "test@gmail.com" || username == "test") && password == "1234") {
+            loginStatus.value = "login"
+        } else {
+            loginStatus.value = "loginFailed"
+        }
+    }
+
+    fun login(account: String, password: String) {
+        if (account == "test@gmail.com" || account == "test") {
+            if (password == "1234") {
+                userData.value = arrayListOf("test@gmail.com", "test", "1234")
+                loginStatus.value = "login"
+            } else {
+                userData.value = arrayListOf("passwordFailed")
+            }
+        } else {
+            userData.value = arrayListOf("accountFailed")
+        }
+    }
 }
