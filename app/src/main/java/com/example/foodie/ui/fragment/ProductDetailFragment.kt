cf99eecfca76941e27c67b50231bbf5f2b7f66d0
tariksafakutuk <tariksafakutuk1905@gmail.com>
2023-11-19 22:25:16 +0300

    :building_construction: Bind ViewModel to product detail fragment

--- app/src/main/java/com/example/foodie/ui/fragment/ProductDetailFragment.kt
+++ app/src/main/java/com/example/foodie/ui/fragment/ProductDetailFragment.kt
@@ -7,18 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.example.foodie.R
 import com.example.foodie.data.entity.CartFood
 import com.example.foodie.databinding.FragmentProductDetailBinding
+import com.example.foodie.datastore.LoginPref
+import com.example.foodie.ui.viewmodel.ProductDetailViewModel
 import com.example.foodie.utils.changePage
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ProductDetailFragment : Fragment() {
     private lateinit var binding: FragmentProductDetailBinding
     private val bundle: ProductDetailFragmentArgs by navArgs()
     private var isFavorite = false
+    private lateinit var viewModel: ProductDetailViewModel
+    private lateinit var loginPref: LoginPref
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,19 +36,39 @@ class ProductDetailFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_product_detail, container, false)
         binding.productDetailFragment = this
 
+        loginPref = LoginPref(requireContext())
+
         isFavorite = bundle.isFavorite
 
         if (isFavorite) {
             binding.ivDetailFavoriteButton.setColorFilter(ContextCompat.getColor(requireContext(), R.color.product_detail_item))
         }
 
+        binding.cartFoodObject = CartFood(1, bundle.food.foodName, bundle.food.foodImageName, bundle.food.foodPrice, 1, "test")
+
         binding.ivDetailFood.setImageResource(
             resources.getIdentifier(bundle.food.foodImageName, "drawable", requireContext().packageName)
         )
 
-        binding.cartFoodObject = CartFood(1, bundle.food.foodName, bundle.food.foodImageName, bundle.food.foodPrice, 1, "test")
+        viewModel.cartFoodObject.observe(viewLifecycleOwner) {
+            binding.cartFoodObject = it
+            binding.invalidateAll()
+        }
+
+        viewModel.addCartStatus.observe(viewLifecycleOwner) {
+            if (it) {
+                Snackbar.make(binding.tvDetailQuantity, "${bundle.food.foodName} ürünü sepete eklendi", Snackbar.LENGTH_SHORT).show()
+                Navigation.changePage(binding.tvDetailQuantity, R.id.action_productDetailFragment_to_cartFragment)
+            }
+        }
 
         return binding.root
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tempViewModel: ProductDetailViewModel by viewModels()
+        viewModel = tempViewModel
     }
 
     fun goToPreviousPage() {
@@ -49,37 +77,30 @@ class ProductDetailFragment : Fragment() {
 
     fun changeFavoriteColor() {
         if (isFavorite) {
+            viewModel.removeFavorite(bundle.food.foodId)
             binding.ivDetailFavoriteButton.setColorFilter(ContextCompat.getColor(requireContext(), R.color.product_detail_favorite))
         } else {
+            viewModel.setFavorite(bundle.food.foodId, bundle.food.foodName, bundle.food.foodImageName, bundle.food.foodPrice)
             binding.ivDetailFavoriteButton.setColorFilter(ContextCompat.getColor(requireContext(), R.color.product_detail_item))
         }
 
         isFavorite = !isFavorite
     }
 
-    fun foodQuantityChange(action: String) {
-        val cardFoodObject = binding.cartFoodObject
-
-        if (cardFoodObject != null) {
-            when (action) {
-                "Decrease" -> {
-                    if (cardFoodObject.foodQuantity != 1) {
-                        cardFoodObject.foodQuantity -= 1
-                    }
-                }
-
-                "Increase" -> {
-                    cardFoodObject.foodQuantity += 1
-                }
-            }
-            cardFoodObject.foodPrice = cardFoodObject.foodQuantity * bundle.food.foodPrice
-            binding.cartFoodObject = cardFoodObject
-            binding.invalidateAll()
-        }
+    fun foodQuantityChange(action: String, cartFoodObject: CartFood) {
+        viewModel.foodQuantityChange(action, cartFoodObject, bundle.food.foodPrice)
     }
 
-    fun addCart(view: View, cartFoodObject: CartFood) {
-        Snackbar.make(view, "${cartFoodObject.foodName} ürünü sepete eklendi", Snackbar.LENGTH_SHORT).show()
-        Navigation.changePage(view, R.id.action_productDetailFragment_to_cartFragment)
+    fun addCart(cartFoodObject: CartFood) {
+        CoroutineScope(Dispatchers.Main).launch {
+            val username = loginPref.readUsername()
+            viewModel.addProductDetailToCart(
+                cartFoodObject.foodName,
+                cartFoodObject.foodImageName,
+                cartFoodObject.foodPrice,
+                cartFoodObject.foodQuantity,
+                username
+            )
+        }
     }
 }
--- app/src/main/java/com/example/foodie/ui/viewmodel/ProductDetailViewModel.kt
+++ app/src/main/java/com/example/foodie/ui/viewmodel/ProductDetailViewModel.kt
@@ -1,6 +1,40 @@
 package com.example.foodie.ui.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.foodie.data.entity.CartFood
 
 class ProductDetailViewModel: ViewModel() {
+    val cartFoodObject = MutableLiveData<CartFood>()
+    val addCartStatus = MutableLiveData<Boolean>()
+
+    fun setFavorite(foodId: Int, foodName: String, foodImageName: String, foodPrice: Int) {
+        Log.e("Message", "Set favorite")
+    }
+
+    fun removeFavorite(foodId: Int) {
+        Log.e("Message", "Remove favorite")
+    }
+
+    fun foodQuantityChange(action: String, tempCartFoodObject: CartFood, foodPrice: Int) {
+        when (action) {
+            "Decrease" -> {
+                if (tempCartFoodObject.foodQuantity != 1) {
+                    tempCartFoodObject.foodQuantity -= 1
+                }
+            }
+
+            "Increase" -> {
+                tempCartFoodObject.foodQuantity += 1
+            }
+        }
+        tempCartFoodObject.foodPrice = tempCartFoodObject.foodQuantity * foodPrice
+        cartFoodObject.value = tempCartFoodObject
+    }
+
+    fun addProductDetailToCart(foodName: String, foodImageName: String, foodPrice: Int, foodQuantity: Int, username: String) {
+        Log.e("Message", "Add food detail to cart")
+        addCartStatus.value = true
+    }
 }
